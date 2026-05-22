c5cb61805a07b28cf25b785332e88ffedda912e9
Animesh Pratap Singh <153443840+AnimeshPratapSingh@users.noreply.github.com>
2026-05-22 12:17:03 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+//  alert(3 + 3) // we are using node js so alert is not defined
+
+let name = "akshat"
+let age = 22
+let isLoggedIn = false
+ 
+console.table([name,age,isLoggedIn])
+
+console.log(typeof name) // string
+console.log(typeof age) // number
+console.log(typeof isLoggedIn) // boolean
+
+// null // standalone value
+// undefined // value is not assigned
+// symbol // unique identifier
+// bigInt // larger than 2^53 - 1
